9ea07aeb47334a6d368093ae755e36e7e383a64a
ashishsah0901 <69725687+ashishsah0901@users.noreply.github.com>
2022-05-03 22:24:15 +0530

    Initial Commit

---
+++ contracts/EthSwap.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./Token.sol";
+
+contract EthSwap {
+    string public name = "EthSwap Instant Exchange";
+    Token public token;
+    uint256 public rate = 100;
+
+    event TokensPurchased(
+        address account,
+        address token,
+        uint256 amount,
+        uint256 rate
+    );
+
+    event TokensSold(
+        address account,
+        address token,
+        uint256 amount,
+        uint256 rate
+    );
+
+    constructor() {
+        token = new Token(1000000000 ether);
+    }
+
+    function buyTokens() public payable {
+        uint256 tokenAmount = msg.value * rate;
+
+        require(token.balanceOf(address(this)) >= tokenAmount);
+
+        token.transfer(msg.sender, tokenAmount);
+
+        emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    function sellTokens(uint256 _amount) public {
+        require(token.balanceOf(msg.sender) >= _amount);
+
+        uint256 etherAmount = _amount / rate;
+
+        require(address(this).balance >= etherAmount);
+
+        token.transferFrom(msg.sender, address(this), _amount);
+        payable(msg.sender).transfer(etherAmount);
+
+        emit TokensSold(msg.sender, address(token), _amount, rate);
+    }
+}
---
+++ test/airdrop.test.js
@@ -0,0 +1,67 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { MerkleTree } = require("merkletreejs");
+const keccak256 = require("keccak256");
+const { arrayify } = require("ethers/lib/utils");
+
+const toWei = (num) => ethers.utils.parseEther(num.toString());
+const fromWei = (num) => ethers.utils.formatEther(num);
+
+describe("AirDrop", function () {
+  const TOKENS_IN_POOL = toWei(1000000000);
+  const REWARD_AMOUNT = toWei(500);
+  let addrs;
+  let contractBlocknumber;
+  const blockNumberCutoff = 11;
+  before(async function () {
+    this.shuffle = [];
+    while (this.shuffle.length < 20) {
+      let r = Math.floor(Math.random() * 20);
+      if (this.shuffle.indexOf(r) === -1) {
+        this.shuffle.push(r);
+      }
+    }
+
+    addrs = await ethers.getSigners();
+    const EthSwapFactory = await ethers.getContractFactory("EthSwap", addrs[0]);
+    this.ethSwap = await EthSwapFactory.deploy();
+    const receipt = await this.ethSwap.deployTransaction.wait();
+    contractBlocknumber = receipt.blockNumber;
+
+    let tokenAddress = await this.ethSwap.token();
+    this.token = (await ethers.getContractFactory("Token", addrs[0])).attach(tokenAddress);
+
+    expect(await this.token.balanceOf(this.ethSwap.address)).to.equal(TOKENS_IN_POOL);
+
+    await Promise.all(
+      this.shuffle.map(async (i, indx) => {
+        const receipt = await (await this.ethSwap.connect(addrs[i]).buyTokens({ value: toWei(10) })).wait();
+        expect(receipt.blockNumber).to.eq(indx + 2);
+      })
+    );
+
+    const filter = this.ethSwap.filters.TokensPurchased();
+    const results = await this.ethSwap.queryFilter(filter, contractBlocknumber, blockNumberCutoff);
+    expect(results.length).to.eq(blockNumberCutoff - contractBlocknumber);
+
+    this.leafNodes = results.map((i) => keccak256(i.args.account.toString()));
+    this.merkleTree = new MerkleTree(this.leafNodes, keccak256, { sortPairs: true });
+    const rootHash = this.merkleTree.getRoot();
+    const AirDropFactory = await ethers.getContractFactory("AirDrop", addrs[0]);
+    this.airDrop = await AirDropFactory.deploy(rootHash, REWARD_AMOUNT);
+  });
+
+  it("Only eligible accounts should be able to claim airdrop", async function () {
+    for (let i = 0; i < 20; i++) {
+      const proof = this.merkleTree.getHexProof(keccak256(addrs[i].address));
+      if (proof.length !== 0) {
+        await this.airDrop.connect(addrs[i]).claim(proof);
+        expect(await this.airDrop.balanceOf(addrs[i].address)).to.eq(REWARD_AMOUNT);
+        await expect(this.airDrop.connect(addrs[i]).claim(proof)).to.be.revertedWith("Already claimed air drop");
+      } else {
+        await expect(this.airDrop.connect(addrs[i]).claim(proof)).to.be.revertedWith("Incorrect merkle proof");
+        expect(await this.airDrop.balanceOf(addrs[i].address)).to.eq(0);
+      }
+    }
+  });
+});
